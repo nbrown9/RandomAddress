6ffc1bb3d29e4895c2f678ce3d024179e5440079
Nick Brown <nlbrown1@icloud.com>
2017-10-15 15:52:01 -0400

    fixed caching issues

--- src/run/Run.java
+++ src/run/Run.java
@@ -8,13 +8,12 @@ import client.Client;
 public class Run {
 
     public static void main (String[] args) throws UnknownHostException {
-    	Security.setProperty("networkaddress.cache.ttl", "0");
-    	Client newClient = new Client();
-        
+    	Security.setProperty("hardwareaddress.cache.ttl", "0");
+    	Client newClient = new Client();    
     	System.out.println(newClient.ClientIP());
-        System.out.println(newClient.ClientOS());
-        System.out.println(newClient.ClientMAC());
-        newClient.ExecuteShellCmd(newClient.ShellCommand(newClient.ClientOS(), newClient.RandomMAC()));
-        
+    System.out.println(newClient.ClientOS());
+    System.out.println(newClient.ClientMAC());
+    System.out.println(newClient.RandomMAC());
+    //newClient.ExecuteShellCmd(newClient.ShellCommand(newClient.ClientOS(), newClient.RandomMAC()));    
     }
 }
